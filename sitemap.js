d003e8c4abafa3365bb238456f1aa6264661e5c7
eoslandvn <eoslandvn@gmail.com>
2019-10-03 20:36:16 +0700

    Update and rename Sitemap.js to sitemap.js

--- sitemap.js
+++ sitemap.js
@@ -16,7 +16,7 @@
    var tocLoaded = false;           // true if feed is read and ToC can be displayed
    var numChars = 250;              // number of characters in post summary
    var postFilter = '';             // default filter value
-   var tocdiv = document.getElementById("son_jovis"); //the toc container
+   var tocdiv = document.getElementById("bp_toc"); //the toc container
    var totalEntires =0; //Entries grabbed till now
    var totalPosts =0; //Total number of posts in the blog.
 
@@ -113,7 +113,7 @@ function loadtoc(json) {
 function filterPosts(filter) {
 // This function changes the filter
 // and displays the filtered list of posts
-  // document.getElementById("son_jovis").scrollTop = document.getElementById("son_jovis").offsetTop;;
+  // document.getElementById("bp_toc").scrollTop = document.getElementById("bp_toc").offsetTop;;
    postFilter = filter;
    displayToc(postFilter);
 } // end filterPosts
